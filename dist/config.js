3aa6c66d16dfd5f526de4b9421da121297e66dd8
mamorenoks <106764684+mamorenoks@users.noreply.github.com>
2022-06-02 15:53:19 -0400

    Add files via upload

--- dist/config.js
+++ dist/config.js
@@ -9,7 +9,7 @@ var _dotenv = require("dotenv");
 
 (0, _dotenv.config)();
 var _default = {
-  port: process.env.PORT || 3000,
+  port: process.env.PORT || 4000,
   dbUser: process.env.DB_USER || "",
   dbPassword: process.env.DB_PASSWORD || "",
   dbServer: process.env.DB_SERVER || "",
