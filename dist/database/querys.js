ef8b21a18ab97d0d6b6cb255bfa8b1a49c4ad3f3
mamorenoks <106764684+mamorenoks@users.noreply.github.com>
2022-06-02 15:42:56 -0400

    Add files via upload

---
+++ dist/database/querys.js
@@ -0,0 +1,10 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.querys = void 0;
+var querys = {
+  getLogin: "	EXECUTE INGRESO @usuario, @pass;"
+};
+exports.querys = querys;
